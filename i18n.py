2843aa7cc224e687c220ecc280e3f827c7779745
Ilias Stamatis <stamatis.iliass@gmail.com>
2013-05-04 13:52:58 +0300

    break long lines

--- i18n.py
+++ i18n.py
@@ -8,10 +8,12 @@ import glob
 
 PROJECT = 'mga3prepo'
 
-os.system("xgettext -L Python --keyword=_ --output=po/{0}.pot {1}".format(PROJECT, PROJECT))
+os.system("xgettext -L Python --keyword=_ --output=po/{0}.pot {0}"\
+          .format(PROJECT))
 for item in glob.glob1("po", "*.po"):
     print("Updating {0}...".format(item))
-    os.system("msgmerge --update --backup=off --no-wrap --sort-by-file po/{0} po/{1}.pot".format(item, PROJECT))
+    os.system("msgmerge --update --backup=off --no-wrap --sort-by-file po/{0} po/{1}.pot"\
+              .format(item, PROJECT))
 
 for po_file in glob.glob1("po", "*.po"):
     lang = po_file[:-3]
@@ -20,4 +22,5 @@ for po_file in glob.glob1("po", "*.po"):
     except:
         print('Directory allready exist')
     print("Installing {0} translations...".format(lang))
-    os.system("msgfmt po/{0}.po -o po/usr/share/locale/{1}/LC_MESSAGES/{2}.mo".format(lang, lang, PROJECT))
+    os.system("msgfmt po/{0}.po -o po/usr/share/locale/{0}/LC_MESSAGES/{1}.mo"\
+              .format(lang, PROJECT))
