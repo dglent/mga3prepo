a5b30502b24d732c850659a3000d2c3d20a6a61b
Dimitrios Glentadakis <dglent@free.fr>
2019-08-24 17:14:35 +0200

    Update translations

--- i18n.py
+++ i18n.py
@@ -23,12 +23,14 @@ import glob
 
 PROJECT = 'mga3prepo'
 
-os.system("xgettext -L Python --keyword=_ --output=po/{0}.pot {0}"\
-          .format(PROJECT))
+os.system(
+    "xgettext -L Python --keyword=_ --output=po/{0}.pot {0}".format(PROJECT)
+)
 for item in glob.glob1("po", "*.po"):
     print("Updating {0}...".format(item))
-    os.system("msgmerge --update --backup=off --no-wrap --sort-by-file po/{0} po/{1}.pot"\
-              .format(item, PROJECT))
+    os.system(
+        "msgmerge --update --backup=off --no-wrap --sort-by-file po/{0} po/{1}.pot".format(item, PROJECT)
+    )
 
 for po_file in glob.glob1("po", "*.po"):
     lang = po_file[:-3]
@@ -37,5 +39,6 @@ for po_file in glob.glob1("po", "*.po"):
     except:
         print('Directory allready exist')
     print("Installing {0} translations...".format(lang))
-    os.system("msgfmt po/{0}.po -o po/usr/share/locale/{0}/LC_MESSAGES/{1}.mo"\
-              .format(lang, PROJECT))
+    os.system(
+        "msgfmt po/{0}.po -o po/usr/share/locale/{0}/LC_MESSAGES/{1}.mo".format(lang, PROJECT)
+    )
